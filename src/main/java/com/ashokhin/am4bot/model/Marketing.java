0cecb7ee90609c774ebcaf7d3b1665b2af43e600
Andrei Shokhin <shokhin.andrey@gmail.com>
2023-09-21 23:15:46 -0400

    Temporaray change marketing company duration

--- src/main/java/com/ashokhin/am4bot/model/Marketing.java
+++ src/main/java/com/ashokhin/am4bot/model/Marketing.java
@@ -1,5 +1,7 @@
 package com.ashokhin.am4bot.model;
 
 public final class Marketing {
-    public static final String MARKETING_COMPANY_REPUTATION_DURATION = "24 Hours";
+    // public static final String MARKETING_COMPANY_REPUTATION_DURATION = "24
+    // Hours";
+    public static final String MARKETING_COMPANY_REPUTATION_DURATION = "4 Hours";
 }
